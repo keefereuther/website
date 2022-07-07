5254286c1ed81f7ee1b5524449c849bb3fec089f
Keefe Reuther <kdreuther@ucsd.edu>
2022-07-07 16:13:00 -0700

    change corrupt fix 7/7

---
+++ code snippets.R
@@ -0,0 +1,8 @@
+library(blogdown)
+blogdown::serve_site()
+blogdown::check_config()
+usethis::browse_github()
+blogdown::check_netlify()
+blogdown::config_netlify()
+blogdown::check_hugo()
+blogdown::config_Rprofile()
